f9fd1d38cf4896828c243a7848a9c8a319dfe88d
TBS <styvebrtchameni@gmail.com>
2026-05-18 15:36:11 +0100

    Add migrate route

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/run-migrate', function() {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations OK';
+});
